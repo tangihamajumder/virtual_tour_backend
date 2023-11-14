20ea4730e0a340d25c2ebc07a318201f88b0ba8d
Md Minhazur Rahman <rafi.rahman03@gmail.com>
2023-11-14 22:35:48 +0600

    added validation in sign-up

--- src/app/modules/auth/auth.routes.js
+++ src/app/modules/auth/auth.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import zodValidationHandler from '../../middlewares/zodValidationHandler.js';
 import { authController } from './auth.controller.js';
+import { createUser } from './auth.validation.js';
 
 const router = express.Router();
 
-router.post('/signup', authController.signUp);
+router.post('/signup', zodValidationHandler(createUser), authController.signUp);
 // router.post('/sign-in',  );
-
 
 export const authRoutes = router;
